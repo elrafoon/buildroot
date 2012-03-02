3e19710c0cb46bebc38530261389e37f94e55bff
Vlado Jendrol <vlado@tind.sk>
2012-03-02 16:24:50 +0100

    Added dependecy on boost.

--- package/SUZI/scl/scl.mk
+++ package/SUZI/scl/scl.mk
@@ -8,7 +8,7 @@ SCL_VERSION = HEAD
 #SCL_SITE = ftp://download.dre.vanderbilt.edu/previous_versions
 SCL_SITE = git://merlin.swind.sk/suzi/suhub-connector-light.git
 SCL_SITE_METHOD = git
-SCL_DEPENDENCIES = ACE 
+SCL_DEPENDENCIES = ACE boost 
 SCL_INSTALL_STAGING = YES
 SCL_INSTALL_TARGET = YES
 SCL_CONF_OPT += -DINSTALL_HEADERS=ON
