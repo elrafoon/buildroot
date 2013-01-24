eab34f93cd38a302e90889c953c28585a7f7cfa2
Stanislav Ravas <ravas@tind.sk>
2013-01-24 12:48:27 +0100

    Add package serial_test
    The serial_test is a simple serial port testing utility.

---
+++ package/serial_test/serial_test.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# SERIAL_TEST
+#
+#############################################################
+SERIAL_TEST_VERSION = master
+SERIAL_TEST_SITE = git://merlin.swind.sk/embedded-tools.git
+SERIAL_TEST_INSTALL_STAGING = NO
+SERIAL_TEST_INSTALL_TARGET = YES
+SERIAL_TEST_DEPENDENCIES = python-serial
+
+SERIAL_TEST_CFGS_$(BR2_PACKAGE_SERIAL_TEST_CFG_IMX28) += serial_test-imx28.conf
+
+define SERIAL_TEST_INSTALL_TARGET_CMDS
+	for i in $(SERIAL_TEST_CFGS_y); do \
+		$(INSTALL) -D -m 0644 $(@D)/serial_test/$$i $(TARGET_DIR)/etc/serial_test.conf; \
+	done
+endef
+
+$(eval $(generic-package))
+
