a119d480b8311f0c4c71862209aa99158a70f42f
Vlado Jendrol <vlado@tind.sk>
2013-03-21 18:46:46 +0100

    dls2_gui: Add package files.

---
+++ package/DLS-2/dls2_gui/dls2_gui.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# DLS2_GUI
+#
+#############################################################
+DLS2_GUI_VERSION = master
+DLS2_GUI_SITE_METHOD = git
+DLS2_GUI_SITE = git://merlin.swind.sk/oms/dls-2.git
+DLS2_GUI_SUBDIR = gui
+DLS2_GUI_INSTALL_STAGING = NO
+DLS2_GUI_INSTALL_TARGET = YES
+DLS2_GUI_DEPENDENCIES = qt dls2_dcs
+DLS2_GUI_SRCDIR = $(@D)/$(DLS2_GUI_SUBDIR)
+
+#define DLS2_GUI_INITSCRIPT
+#	$(INSTALL) -D -m 0755 $(DLS2_GUI_SRCDIR)/initscript-dls2_hwtest $(TARGET_DIR)/etc/init.d/S90dls2_hwtest
+#endef
+
+DLS2_CONF_OPT += -DTARGET_PLATFORM=T20
+DLS2_CONF_OPT += -DHIDE_WINDOW_DECORATION
+
+define DLS2_GUI_INITSCRIPT
+        $(INSTALL) -D -m 0755 $(DLS2_GUI_SRCDIR)/etc/initscript-dls2_gui $(TARGET_DIR)/etc/init.d/S90dls2_gui
+endef
+
+
+DLS2_GUI_POST_INSTALL_TARGET_HOOKS += DLS2_GUI_INITSCRIPT
+
+$(eval $(cmake-package))
+
