669ac00f685c96de41cec7e01e48bdd24cf2f986
Stanislav Ravas <ravas@tind.sk>
2013-03-21 20:08:15 +0100

    dls2_gui: Append GUI service to DCS svc.conf

--- package/DLS-2/dls2_gui/dls2_gui.mk
+++ package/DLS-2/dls2_gui/dls2_gui.mk
@@ -12,10 +12,6 @@ DLS2_GUI_INSTALL_TARGET = YES
 DLS2_GUI_DEPENDENCIES = qt dls2_dcs
 DLS2_GUI_SRCDIR = $(@D)/$(DLS2_GUI_SUBDIR)
 
-#define DLS2_GUI_INITSCRIPT
-#	$(INSTALL) -D -m 0755 $(DLS2_GUI_SRCDIR)/initscript-dls2_hwtest $(TARGET_DIR)/etc/init.d/S90dls2_hwtest
-#endef
-
 DLS2_CONF_OPT += -DTARGET_PLATFORM=T20
 DLS2_CONF_OPT += -DHIDE_WINDOW_DECORATION
 
@@ -23,8 +19,12 @@ define DLS2_GUI_INITSCRIPT
         $(INSTALL) -D -m 0755 $(DLS2_GUI_SRCDIR)/etc/initscript-dls2_gui $(TARGET_DIR)/etc/init.d/S90dls2_gui
 endef
 
+define DLS2_GUI_SVC_CONF
+	grep GUI $(TARGET_DIR)/etc/dcs/svc.conf || echo 'dynamic GUI Service_Object *dcs-gui-svc:_make_GUI() "-Q /usr/share/dcs/ -- -qws"' >>$(TARGET_DIR)/etc/dcs/svc.conf
+endef
 
 DLS2_GUI_POST_INSTALL_TARGET_HOOKS += DLS2_GUI_INITSCRIPT
+DLS2_GUI_POST_INSTALL_TARGET_HOOKS += DLS2_GUI_SVC_CONF
 
 $(eval $(cmake-package))
 
