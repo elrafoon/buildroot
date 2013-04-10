ab7ec34d06d72d93d2724141819b89ab3d0b1704
Stanislav Ravas <ravas@tind.sk>
2013-04-10 13:32:02 +0200

    ACE: Update to version 6.1.7
    Improved menu.

--- package/ACE/config.h
+++ package/ACE/config.h
@@ -1,12 +1,21 @@
+#include <ace/config-linux.h>
+
 #define _LARGEFILE_SOURCE 1
 #define _FILE_OFFSET_BITS 64
 
+#ifndef ACE_LACKS_STROPTS_H
 #define ACE_LACKS_STROPTS_H 1
+#endif
+
+#ifndef ACE_HAS_CPU_SET_T
 #define ACE_HAS_CPU_SET_T 1
+#endif
+
+#ifndef ACE_LACKS_STRRECVFD
 #define ACE_LACKS_STRRECVFD 1
+#endif
 //#define ACE_LACKS_WCTYPE_H 1
 
-#include <ace/config-linux.h>
 //#undef ACE_HAS_XPG4_MULTIBYTE_CHAR
 //#undef ACE_HAS_LSEEK64
 #undef ACE_SCANDIR_CMP_USES_VOIDPTR
