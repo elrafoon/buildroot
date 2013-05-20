2658499f1cf1ada94ce292446c3c1de32bdbaa83
Stanislav Ravas <ravas@tind.sk>
2013-05-20 13:51:14 +0200

    dls2_dcsconfig: New package
    DCS configuration generator support module.

---
+++ package/DLS-2/dls2_dcsconfig/dls2_dcsconfig.mk
@@ -0,0 +1,27 @@
+DLS2_DCSCONFIG_VERSION = master
+DLS2_DCSCONFIG_SITE = git://merlin.swind.sk/oms/dls-2.git
+DLS2_DCSCONFIG_SUBDIR = dcsconfig
+DLS2_DCSCONFIG_SRCDIR = $(@D)/$(DLS2_DCS_SUBDIR)
+DLS2_DCSCONFIG_DEPENDENCIES = host-python
+
+define HOST_DLS2_DCSCONFIG_BUILD_CMDS
+        (cd $(DLS2_DCSCONFIG_SRCDIR); $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+
+define DLS2_DCSCONFIG_BUILD_CMDS
+        (cd $(DLS2_DCSCONFIG_SRCDIR); $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+
+define HOST_DLS2_DCSCONFIG_INSTALL_CMDS
+        (cd $(DLS2_DCSCONFIG_SRCDIR); PYTHONPATH="$(HOST_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
+        $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(HOST_DIR)/usr)
+endef
+
+define DLS2_DCSCONFIG_INSTALL_TARGET_CMDS
+        (cd $(DLS2_DCSCONFIG_SRCDIR); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
+        $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
+
