fa10936a632c4c96ec4cdf81da907bc279f34c1f
Stanislav Ravas <ravas@tind.sk>
2013-06-25 15:43:35 +0200

    surpc: Build python module as option
    SuRPC python module can be built when selected.

--- package/SUZI/surpc/surpc.mk
+++ package/SUZI/surpc/surpc.mk
@@ -9,9 +9,11 @@ SURPC_SITE_METHOD = git
 SURPC_DEPENDENCIES = ACE protobuf
 SURPC_INSTALL_STAGING = YES
 SURPC_INSTALL_TARGET = YES
+SURPC_CONF_OPT += -DBUILD_PYTHON=$(if $(BR2_PACKAGE_SURPC_PYTHON),ON,OFF)
 
 define SURPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(STAGING_DIR)/usr/lib/libSuRPC*.so $(TARGET_DIR)/usr/lib
+	(cd $(@D)/python; $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
 endef
 
 $(eval $(call cmake-package))
