0f4c963bfbd72be810b54f53d39657f0df082fb4
Stanislav Ravas <ravas@tind.sk>
2013-08-16 09:43:55 +0200

    monitor: Bump version to 3877342

--- package/adroctech/monitor/monitor.mk
+++ package/adroctech/monitor/monitor.mk
@@ -3,7 +3,7 @@
 # monitor
 #
 #############################################################
-MONITOR_VERSION = 02f6521
+MONITOR_VERSION = 3877342
 MONITOR_SITE = /home/stano/adroctech/monitor
 MONITOR_SITE_METHOD = git
 MONITOR_DEPENDENCIES = ACE pcre jpeg opencv qt
