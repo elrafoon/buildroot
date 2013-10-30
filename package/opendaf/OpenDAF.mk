a0551573a992137e720388fff90a1e60903f175b
Stanislav Ravas <ravas@tind.sk>
2013-10-30 23:52:11 +0100

    opendaf: Install profile script to export PYTHONPATH
    Package installs python modules to nonstandard location, which
    must be included in PYTHONPATH.

--- package/opendaf/OpenDAF.mk
+++ package/opendaf/OpenDAF.mk
@@ -22,10 +22,15 @@ define OPENDAF_INSTALL_INITSCRIPT
 	$(INSTALL) -D -m 0755 package/opendaf/S90opendaf $(TARGET_DIR)/etc/init.d/S90opendaf
 endef
 
+define OPENDAF_INSTALL_PROFILE
+	$(INSTALL) -D -m 0644 package/opendaf/opendaf_pythonpath.sh $(TARGET_DIR)/etc/profile.d/opendaf_pythonpath.sh
+endef
+
 define OPENDAF_INSTALL_RUNIT_SERVICE
 	$(INSTALL) -D -m 0755 package/opendaf/runit-run $(TARGET_DIR)/etc/service/opendaf/run
 endef
 
+OPENDAF_POST_INSTALL_TARGET_HOOKS += OPENDAF_INSTALL_PROFILE
 ifeq ($(BR2_PACKAGE_OPENDAF_STARTUP_INITSCRIPT),y)
 	OPENDAF_POST_INSTALL_TARGET_HOOKS += OPENDAF_INSTALL_INITSCRIPT
 else ifeq ($(BR2_PACKAGE_OPENDAF_STARTUP_RUNIT),y)
---
+++ package/opendaf/opendaf_pythonpath.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH='/usr/share/opendaf/templates/modules/'
+
