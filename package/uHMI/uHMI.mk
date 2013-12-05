d653e70b47859713f945e12e31ee0b313d0818a6
Stanislav Ravas <ravas@tind.sk>
2013-12-05 13:00:46 +0100

    uHMI: Configurable scan period

--- package/uHMI/uHMI.mk
+++ package/uHMI/uHMI.mk
@@ -2,6 +2,7 @@ UHMI_VERSION = master
 UHMI_SITE = ssh://merlin.swind.sk:29418/uHMI
 UHMI_SITE_METHOD = git
 UHMI_DEPENDENCIES = opendaf
+UHMI_CONF_OPT = -DSCAN_PERIOD=$(BR2_PACKAGE_UHMI_SCAN_PERIOD)
 
 $(eval $(cmake-package))
 
