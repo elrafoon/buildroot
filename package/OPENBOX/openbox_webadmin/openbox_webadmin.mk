9740d5671be01800a953adb78057917cd8ac8c86
Vlado Jendroľ <vlado@tind.sk>
2014-03-13 16:51:31 +0100

    openbox/webadmin: Fix title customization.
    Force title tag substitution by deleting file from target before installation.

--- package/OPENBOX/openbox_webadmin/openbox_webadmin.mk
+++ package/OPENBOX/openbox_webadmin/openbox_webadmin.mk
@@ -20,12 +20,13 @@ define OPENBOX_WEBADMIN_BUILD_CMDS
 endef
 
 define OPENBOX_WEBADMIN_INSTALL_TARGET_CMDS
-        (cd $(@D)/$(OPENBOX_WEBADMIN_MODEL_SUBDIR); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
+    (cd $(@D)/$(OPENBOX_WEBADMIN_MODEL_SUBDIR); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
         make $(OPENBOX_WEBADMIN_OPTIONS) install)
 
 	(cd $(@D)/$(OPENBOX_WEBADMIN_RENDERER_SUBDIR); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
         make $(OPENBOX_WEBADMIN_OPTIONS) install)
 
+	rm -f $(TARGET_DIR)/usr/share/openbox/web/html/base.html
 	(cd $(@D)/$(OPENBOX_WEBADMIN_WEB_SUBDIR); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
         make $(OPENBOX_WEBADMIN_OPTIONS) install)
 	sed -i s/@PRODUCT_NAME@/$(BR2_PACKAGE_OPENBOX_WEBADMIN_TITLE)/ $(TARGET_DIR)/usr/share/openbox/web/html/base.html
