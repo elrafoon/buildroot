1ed1275ec09d56dfe428f0dfadc5cd173a3fa5d8
Stanislav Ravas <ravas@tind.sk>
2014-05-14 17:45:26 +0200

    ACE: Fix build with glibc

--- package/ACE/config.h
+++ package/ACE/config.h
@@ -3,33 +3,42 @@
 
 #include <ace/config-linux.h>
 
-#ifndef ACE_LACKS_STROPTS_H
-#define ACE_LACKS_STROPTS_H 1
-#endif
+#ifdef __UCLIBC__
+    #include <ace/config-linux.h>
 
-#ifndef ACE_HAS_CPU_SET_T
-#define ACE_HAS_CPU_SET_T 1
-#endif
+    #ifndef ACE_LACKS_STROPTS_H
+    #define ACE_LACKS_STROPTS_H 1
+    #endif
 
-#ifndef ACE_LACKS_STRRECVFD
-#define ACE_LACKS_STRRECVFD 1
-#endif
-//#define ACE_LACKS_WCTYPE_H 1
+    #ifndef ACE_HAS_CPU_SET_T
+    #define ACE_HAS_CPU_SET_T 1
+    #endif
 
-//#undef ACE_HAS_XPG4_MULTIBYTE_CHAR
-//#undef ACE_HAS_LSEEK64
-#undef ACE_SCANDIR_CMP_USES_VOIDPTR
-#undef ACE_SCANDIR_CMP_USES_CONST_VOIDPTR
-#undef ACE_HAS_AIO_CALLS
-#undef ACE_HAS_STREAMS
-#undef ACE_HAS_STREAM_PIPES
+    #ifndef ACE_LACKS_STRRECVFD
+    #define ACE_LACKS_STRRECVFD 1
+    #endif
+    //#define ACE_LACKS_WCTYPE_H 1
+
+    //#undef ACE_HAS_XPG4_MULTIBYTE_CHAR
+    //#undef ACE_HAS_LSEEK64
+    #undef ACE_SCANDIR_CMP_USES_VOIDPTR
+    #undef ACE_SCANDIR_CMP_USES_CONST_VOIDPTR
+    #undef ACE_HAS_AIO_CALLS
+    #undef ACE_HAS_STREAMS
+    #undef ACE_HAS_STREAM_PIPES
+
+    #define ACE_HAS_TIMED_MESSAGE_BLOCKS
+
+    /*
+     *  UCLIBC OPTIONS:
+
+    UCLIBC_SUPPORT_AI_ADDRCONFIG=y
+    UCLIBC_HAS_BACKTRACE=y
+    
+    */
+#else   // other C-library implementations
+    // nothing special
+#endif
 
 #define ACE_HAS_TIMED_MESSAGE_BLOCKS
 
-/*
- *  UCLIBC OPTIONS:
-
-UCLIBC_SUPPORT_AI_ADDRCONFIG=y
-UCLIBC_HAS_BACKTRACE=y
-
-*/
