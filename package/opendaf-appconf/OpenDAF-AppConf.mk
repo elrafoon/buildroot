d296eee602cfc247105caf1769dd60935547fa04
Stanislav Ravas <ravas@tind.sk>
2014-05-14 18:35:09 +0200

    opendaf-appconf: Application-specific OpenDAF configurations
    Installs chosen application-specific OpenDAF configuration.

---
+++ package/opendaf-appconf/OpenDAF-AppConf.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# OPENDAF_APPCONF
+#
+#############################################################
+OPENDAF_APPCONF_VERSION = $(BR2_PACKAGE_OPENDAF_APPCONF_NAME)
+OPENDAF_APPCONF_SITE_METHOD = git
+OPENDAF_APPCONF_SITE = ssh://merlin.swind.sk:29418/OpenDAF-AppConf.git
+OPENDAF_APPCONF_INSTALL_STAGING = NO
+OPENDAF_APPCONF_INSTALL_TARGET = YES
+OPENDAF_APPCONF_DEPENDENCIES = opendaf
+
+define OPENDAF_APPCONF_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(generic-package))
+
