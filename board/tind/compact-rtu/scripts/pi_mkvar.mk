f7e4c4e1f1e863f38ca4d35e83fee33878f7106d
Stanislav Ravas <ravas@tind.sk>
2014-06-04 19:40:20 +0200

    tind_compactio_defconfig: Split rootfs into / and /var
    Patch is somewhat dirty due to using sudo, but likely it is the
    only non-invasive way to accomplish the task.

---
+++ board/tind/compact-rtu/scripts/pi_mkvar.mk
@@ -0,0 +1,45 @@
+include $(BUILDROOT_CONFIG)
+
+MKFS.UBIFS = $(HOST_DIR)/usr/sbin/mkfs.ubifs
+
+UBIFS_OPTS := -e $(BR2_TARGET_ROOTFS_UBIFS_LEBSIZE) -m $(BR2_TARGET_ROOTFS_UBIFS_MINIOSIZE)
+
+ifeq ($(BR2_TARGET_ROOTFS_UBIFS_RT_ZLIB),y)
+UBIFS_OPTS += -x zlib
+endif
+ifeq ($(BR2_TARGET_ROOTFS_UBIFS_RT_LZO),y)
+UBIFS_OPTS += -x lzo
+endif
+ifeq ($(BR2_TARGET_ROOTFS_UBIFS_RT_NONE),y)
+UBIFS_OPTS += -x none
+endif
+
+UBIFS_OPTS += $(call qstrip,$(BR2_TARGET_ROOTFS_UBIFS_OPTS))
+
+all: $(BINARIES_DIR)/rootfs-only.tar $(BINARIES_DIR)/rootfs-only.ubifs $(BINARIES_DIR)/var.tar $(BINARIES_DIR)/var.ubifs
+
+
+$(BINARIES_DIR)/rootfs-only.tar:	$(BINARIES_DIR)/rootfs.tar
+									echo "Creating $@"
+									cp $< $@
+									tar --delete -f $@ ./var
+
+$(BINARIES_DIR)/rootfs-only.ubifs:	$(BINARIES_DIR)/rootfs-only.tar
+									echo "Creating $@"
+									sudo tar -xf $< -C $(TMPDIR)
+									sudo $(MKFS.UBIFS) -d $(TMPDIR) $(UBIFS_OPTS) -c $(FS_ROOT_SIZE_LEBS) -o $@
+									sudo rm -rf $(TMPDIR)/*
+
+$(BINARIES_DIR)/var.tar:	$(BINARIES_DIR)/rootfs.tar
+							echo "Creating $@"
+							cp $< $@
+							tar --delete -f $@ $$(tar -tf $@ | grep -v ^./var | egrep ^./[[:alnum:]]+/*$$)
+
+$(BINARIES_DIR)/var.ubifs:	$(BINARIES_DIR)/var.tar
+							echo "Creating $@"
+							sudo tar -xf $< -C $(TMPDIR)
+							sudo $(MKFS.UBIFS) -d $(TMPDIR) $(UBIFS_OPTS) -c $(FS_VAR_SIZE_LEBS) -o $@
+							sudo rm -rf $(TMPDIR)/*
+
+.PHONY: all
+
