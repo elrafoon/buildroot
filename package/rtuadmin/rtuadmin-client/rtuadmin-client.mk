76a5c08b3a113e92701fc7bb12be0536ae2e094d
Stanislav Ravas <ravas@tind.sk>
2014-08-07 22:49:51 +0200

    rtuadmin-*: Group into RTUAdmin submenu

--- package/rtuadmin/rtuadmin-client/rtuadmin-client.mk
+++ package/rtuadmin/rtuadmin-client/rtuadmin-client.mk
@@ -3,15 +3,16 @@
 # RTUADMIN_CLIENT
 #
 #############################################################
-RTUADMIN_CLIENT_VERSION = master
+RTUADMIN_CLIENT_VERSION = $(BR2_PACKAGE_RTUADMIN_VERSION)
 RTUADMIN_CLIENT_SITE = http://merlin/packages/r
-RTUADMIN_CLIENT_SOURCE = rtuadmin-client.tar.bz2
+RTUADMIN_CLIENT_SOURCE = rtuadmin-client-$(RTUADMIN_CLIENT_VERSION).tar.bz2
 RTUADMIN_CLIENT_INSTALL_STAGING = NO
 RTUADMIN_CLIENT_INSTALL_TARGET = YES
 
 define RTUADMIN_CLIENT_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/rtuadmin-client
 	tar xf $(@D)/build.tar.bz2 --strip-components=1 -C $(TARGET_DIR)/usr/share/rtuadmin-client
+	mkdir -p $(TARGET_DIR)/var/www/
 	ln -sf /usr/share/rtuadmin-client/ $(TARGET_DIR)/var/www/rtuadmin-client
 endef
 
--- package/rtuadmin/rtuadmin-server/rtuadmin-server.mk
+++ package/rtuadmin/rtuadmin-server/rtuadmin-server.mk
@@ -3,17 +3,17 @@
 # RTUADMIN_SERVER
 #
 #############################################################
-RTUADMIN_SERVER_VERSION = master
+RTUADMIN_SERVER_VERSION = $(BR2_PACKAGE_RTUADMIN_VERSION)
 RTUADMIN_SERVER_SITE_METHOD = git
 RTUADMIN_SERVER_SITE = ssh://gitolite@merlin/rtuadmin-server
 RTUADMIN_SERVER_INSTALL_STAGING = NO
 RTUADMIN_SERVER_INSTALL_TARGET = YES
 RTUADMIN_SERVER_DEPENDENCIES = cjson lua luacosmo sqlite
-RTUADMIN_SERVER_CONF_OPT += -DHTTPD_VERSION_HASH=$(RTUADMIN_SERVER_VERSION) -DOPENDAF_TEMPLATES="/usr/share/opendaf/templates/"
+RTUADMIN_SERVER_CONF_OPT += -DCMAKE_C_FLAGS="-O0 -g3" -DHTTPD_VERSION_HASH=$(RTUADMIN_SERVER_VERSION) -DOPENDAF_TEMPLATES="/usr/share/opendaf/templates/"
 
 define RTUADMIN_SERVER_INSTALL_INITSCRIPT
-    $(INSTALL) -D -m 0755 package/rtuadmin-server/S00rtuadmin-renderer $(TARGET_DIR)/etc/init.d/S00rtuadmin-renderer
-    $(INSTALL) -D -m 0755 package/rtuadmin-server/S95rtuadmin-server $(TARGET_DIR)/etc/init.d/S95rtuadmin-server
+    $(INSTALL) -D -m 0755 package/rtuadmin/rtuadmin-server/S00rtuadmin-renderer $(TARGET_DIR)/etc/init.d/S00rtuadmin-renderer
+    $(INSTALL) -D -m 0755 package/rtuadmin/rtuadmin-server/S95rtuadmin-server $(TARGET_DIR)/etc/init.d/S95rtuadmin-server
 endef
 
 # setup target installation hooks
