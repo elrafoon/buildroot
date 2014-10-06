fd10617bfa1768372f2cbdfbd2efb7fa40d0bd23
Stanislav Ravas <ravas@tind.sk>
2014-10-06 22:21:24 +0200

    opendaf-appconf: Update upstream url

--- package/opendaf-appconf/OpenDAF-AppConf.mk
+++ package/opendaf-appconf/OpenDAF-AppConf.mk
@@ -5,7 +5,7 @@
 #############################################################
 OPENDAF_APPCONF_VERSION = $(BR2_PACKAGE_OPENDAF_APPCONF_NAME)
 OPENDAF_APPCONF_SITE_METHOD = git
-OPENDAF_APPCONF_SITE = ssh://merlin.swind.sk:29418/OpenDAF-AppConf.git
+OPENDAF_APPCONF_SITE = gitolite@merlin.swind.sk:OpenDAF-AppConf.git
 OPENDAF_APPCONF_INSTALL_STAGING = NO
 OPENDAF_APPCONF_INSTALL_TARGET = YES
 OPENDAF_APPCONF_DEPENDENCIES = opendaf
