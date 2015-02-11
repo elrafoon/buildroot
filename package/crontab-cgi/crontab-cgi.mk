2ed9720151012cc8292f3e781e8d7024d40dd3b0
Stanislav Ravas <ravas@tind.sk>
2015-02-11 15:43:07 +0100

    crontab-cgi: New package
    CGI script to manipulate user crontab.

---
+++ package/crontab-cgi/crontab-cgi.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# crontab-cgi
+#
+################################################################################
+
+CRONTAB_CGI_VERSION = 1e956a2
+CRONTAB_CGI_SITE_METHOD = git
+CRONTAB_CGI_SITE = gitolite@merlin.swind.sk:embedded-tools.git
+CRONTAB_CGI_LICENSE = BSD-3c
+CRONTAB_CGI_SUBDIR = crontab-cgi
+
+define CRONTAB_CGI_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/$(CRONTAB_CGI_SUBDIR)/crontab-cgi.sh $(TARGET_DIR)/usr/libexec/cgi-bin/crontab
+endef
+
+$(eval $(generic-package))
+
