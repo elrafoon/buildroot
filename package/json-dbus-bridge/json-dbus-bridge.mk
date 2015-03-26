edf6caf8e50cb45968efb1d546b92285f8ab684e
Stanislav Ravas <ravas@tind.sk>
2015-03-26 19:00:02 +0100

    json-dbus-bridge: Bump to v1.1.5
    Fixed upstream.

--- package/json-dbus-bridge/json-dbus-bridge.mk
+++ package/json-dbus-bridge/json-dbus-bridge.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-JSON_DBUS_BRIDGE_VERSION = v1.1.3
-JSON_DBUS_BRIDGE_SITE = git://gitorious.org/json-dbus-bridge/json-dbus-bridge.git
+JSON_DBUS_BRIDGE_VERSION = v1.1.5
+JSON_DBUS_BRIDGE_SITE = https://gitorious.org/json-dbus-bridge/json-dbus-bridge.git
 JSON_DBUS_BRIDGE_SITE_METHOD = git
 JSON_DBUS_BRIDGE_LICENSE = LGPLv2.1
 JSON_DBUS_BRIDGE_LICENSE_FILES = COPYING
