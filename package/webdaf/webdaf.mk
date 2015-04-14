d7892cdd83ddf81aa85e011b0da964a2d22f65a7
Stanislav Ravas <ravas@tind.sk>
2015-04-14 12:24:36 +0200

    webdaf: OpenDAF web management interface

---
+++ package/webdaf/webdaf.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# WEBDAF
+#
+#############################################################
+WEBDAF_VERSION = be08e70
+WEBDAF_SITE = http://merlin/packages/w
+WEBDAF_SOURCE = webdaf-$(WEBDAF_VERSION).tar.bz2
+WEBDAF_INSTALL_STAGING = NO
+WEBDAF_INSTALL_TARGET = YES
+
+define WEBDAF_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/webdaf
+	cp -rf $(@D)/* $(TARGET_DIR)/usr/share/webdaf/
+	mkdir -p $(TARGET_DIR)/var/www/
+	ln -sf /usr/share/webdaf/ $(TARGET_DIR)/var/www/webdaf
+endef
+
+$(eval $(generic-package))
