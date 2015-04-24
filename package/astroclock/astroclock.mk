9c87d3d17e5dfc04be02883e4c67d1b5f44ea1f0
Stanislav Ravas <ravas@tind.sk>
2015-04-24 16:09:19 +0200

    astroclock: New package
    Astronomous clock based task scheduler similar to cron.

---
+++ package/astroclock/astroclock.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# astroclock
+#
+################################################################################
+
+ASTROCLOCK_VERSION = 81e5944
+ASTROCLOCK_SITE_METHOD = git
+ASTROCLOCK_SITE = gitolite@merlin.swind.sk:embedded-tools.git
+ASTROCLOCK_LICENSE = BSD-3c
+ASTROCLOCK_SUBDIR = astroclock
+ASTROCLOCK_DEPENDENCIES = dcron
+
+define ASTROCLOCK_INSTALL_TARGET_CGI_$(BR2_PACKAGE_ASTROCLOCK_CGI)
+	$(INSTALL) -D -m 0755 $(@D)/$(ASTROCLOCK_SUBDIR)/astro-cgi.sh $(TARGET_DIR)/usr/libexec/cgi-bin/astro
+endef
+
+ASTROCLOCK_POST_INSTALL_TARGET_HOOKS += ASTROCLOCK_INSTALL_TARGET_CGI_y
+
+define ASTROCLOCK_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(@D)/$(ASTROCLOCK_SUBDIR)/astroclock.service $(TARGET_DIR)/etc/systemd/system/astroclock.service;
+	ln -fs ../astroclock.service $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/astroclock.service;
+endef
+
+$(eval $(cmake-package))
+
