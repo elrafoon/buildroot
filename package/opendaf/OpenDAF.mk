34e047b10fd45ff7a2778a795ac3336761924c2c
Stanislav Ravas <ravas@tind.sk>
2015-07-07 18:14:28 +0200

    opendaf: Bump to 760c084
    Fixes modbus-tcp-master connector errors.

--- package/opendaf/OpenDAF.mk
+++ package/opendaf/OpenDAF.mk
@@ -3,7 +3,7 @@
 # OPENDAF
 #
 #############################################################
-OPENDAF_VERSION = 89745ed
+OPENDAF_VERSION = 760c084
 OPENDAF_SITE_METHOD = git
 OPENDAF_SITE = gitolite@merlin.swind.sk:OpenDAF.git
 OPENDAF_INSTALL_STAGING = NO
