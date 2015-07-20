f4c714c93e22e995ec40b71aea57dcdf896c983f
Stanislav Ravas <ravas@tind.sk>
2015-07-20 23:17:39 +0200

    opendaf: Bump to 768a8bf

--- package/opendaf/OpenDAF.mk
+++ package/opendaf/OpenDAF.mk
@@ -3,7 +3,7 @@
 # OPENDAF
 #
 #############################################################
-OPENDAF_VERSION = 760c084
+OPENDAF_VERSION = 768a8bf
 OPENDAF_SITE_METHOD = git
 OPENDAF_SITE = gitolite@merlin.swind.sk:OpenDAF.git
 OPENDAF_INSTALL_STAGING = NO
