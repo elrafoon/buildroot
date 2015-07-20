01301fa3285552b6a038aea25665761bddeff1fc
Stanislav Ravas <ravas@tind.sk>
2015-07-20 23:05:59 +0200

    rtuadmin-rang: Bump to 7aedbec

--- package/rtuadmin/rtuadmin-rang/rang.mk
+++ package/rtuadmin/rtuadmin-rang/rang.mk
@@ -4,19 +4,21 @@
 #
 ################################################################################
 
-RTUADMIN_RANG_VERSION = 39c0719
+RTUADMIN_RANG_VERSION = 7aedbec
 RTUADMIN_RANG_SITE_METHOD = git
 RTUADMIN_RANG_SITE = gitolite@merlin.swind.sk:rtuadmin-rang.git
 
 define RTUADMIN_RANG_INSTALL_TARGET_RTUADMIN_RANG
 	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
-	$(INSTALL) -D -m 0644 $(TARGET_DIR)/var/lib/rang/cfg.json $(TARGET_DIR)/usr/share/rang/cfg.json
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/systemd/system/basic.target.wants/
 	$(INSTALL) -D -m 0644 package/rtuadmin/rtuadmin-rang/rang-render.service $(TARGET_DIR)/etc/systemd/system/rang-render.service
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/basic.target.wants
 	ln -sf ../rang-render.service $(TARGET_DIR)/etc/systemd/system/basic.target.wants/rang-render.service
+	$(INSTALL) -D -m 0644 package/rtuadmin/rtuadmin-rang/rang-install.service $(TARGET_DIR)/etc/systemd/system/rang-install.service
+	ln -sf ../rang-install.service $(TARGET_DIR)/etc/systemd/system/basic.target.wants/rang-install.service
 endef
 
 define RTUADMIN_RANG_INSTALL_TARGET_RTUADMIN_RANG_CGI_$(BR2_PACKAGE_RTUADMIN_RANG_CGI)
+	mkdir -p $(TARGET_DIR)/usr/libexec/cgi-bin/
 	ln -sf /usr/bin/rang-cgi $(TARGET_DIR)/usr/libexec/cgi-bin/rang
 endef
 
