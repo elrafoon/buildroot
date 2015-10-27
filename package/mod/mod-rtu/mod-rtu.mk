ecd9eedf10bd70dce7e8a7a94229caf171445ed8
Stanislav Ravas <ravas@tind.sk>
2015-10-27 17:40:38 +0100

    mod-rtu: Split overlay to common and machine-specific parts

--- package/mod/mod-rtu/mod-rtu.mk
+++ package/mod/mod-rtu/mod-rtu.mk
@@ -11,7 +11,8 @@ MOD_RTU_INSTALL_TARGET = YES
 MOD_RTU_DEPENDENCIES = opendaf opendaf-appconf lighttpd network-manager
 
 define MOD_RTU_INSTALL_TARGET_CMDS
-	rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/ $(TARGET_DIR)/
+	rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/common/ $(TARGET_DIR)/
+	[ ! -z $(BR2_MACHINE) ] && [ -d $(@D)/overlay/machine/$(BR2_MACHINE)/ ] && rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/machine/$(BR2_MACHINE)/ $(TARGET_DIR)/
 endef
 
 $(eval $(generic-package))
