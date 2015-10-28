b44dab6a4546d9ee4dfe3d98ce0e60d48cc41121
Stanislav Ravas <ravas@tind.sk>
2015-10-28 13:12:04 +0100

    systemtest: New package

---
+++ package/systemtest/systemtest.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# systemtest
+#
+################################################################################
+
+SYSTEMTEST_SITE = gitolite@merlin.swind.sk:embedded-tools.git
+SYSTEMTEST_SITE_METHOD = git
+SYSTEMTEST_VERSION = 47bcd60
+
+define SYSTEMTEST_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D)/systemtest			\
+		DESTDIR="$(TARGET_DIR)" 		\
+		MACHINE=$(BR2_MACHINE)			\
+		install
+endef
+
+$(eval $(generic-package))
+
