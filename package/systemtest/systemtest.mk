f653e97e6c93ef2e13e23e929232316d42d17a57
Stanislav Ravas <ravas@tind.sk>
2015-11-12 20:48:46 +0100

    systemtest: Bump to 9b3834e

--- package/systemtest/systemtest.mk
+++ package/systemtest/systemtest.mk
@@ -6,7 +6,7 @@
 
 SYSTEMTEST_SITE = gitolite@merlin.swind.sk:embedded-tools.git
 SYSTEMTEST_SITE_METHOD = git
-SYSTEMTEST_VERSION = 5cb4df7
+SYSTEMTEST_VERSION = 9b3834e
 
 define SYSTEMTEST_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D)/systemtest			\
