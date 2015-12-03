9f494d8567a7451b8e77ca4a5b4d31181a5325c0
Stanislav Ravas <ravas@tind.sk>
2015-12-03 14:54:06 +0100

    systemtest: Bump to 316f4cf

--- package/systemtest/systemtest.mk
+++ package/systemtest/systemtest.mk
@@ -6,7 +6,7 @@
 
 SYSTEMTEST_SITE = gitolite@merlin.swind.sk:embedded-tools.git
 SYSTEMTEST_SITE_METHOD = git
-SYSTEMTEST_VERSION = 290c395
+SYSTEMTEST_VERSION = 316f4cf
 
 define SYSTEMTEST_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D)/systemtest			\
