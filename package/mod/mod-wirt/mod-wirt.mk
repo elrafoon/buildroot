d7ff0c69d1779320ae6f389bdb180d15128ccd5d
Stanislav Ravas <ravas@tind.sk>
2016-01-05 20:27:29 +0100

    mod-wirt: Delimit install commands with semicolons
    Commands were not executed as expected.

--- package/mod/mod-wirt/mod-wirt.mk
+++ package/mod/mod-wirt/mod-wirt.mk
@@ -15,12 +15,12 @@ MOD_WIRT_DEPENDENCIES += mod-rtu
 endif
 
 define MOD_WIRT_INSTALL_TARGET_CMDS_BASE
-	rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/common/ $(TARGET_DIR)/
-	[ ! -z $(BR2_MACHINE) ] && [ -d $(@D)/overlay/machine/$(BR2_MACHINE)/ ] && rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/machine/$(BR2_MACHINE)/ $(TARGET_DIR)/ || true
+	rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/common/ $(TARGET_DIR)/;
+	[ ! -z $(BR2_MACHINE) ] && [ -d $(@D)/overlay/machine/$(BR2_MACHINE)/ ] && rsync -a --force --ignore-times --chmod=Du+w --exclude .empty --exclude '*~' $(@D)/overlay/machine/$(BR2_MACHINE)/ $(TARGET_DIR)/ || true;
 endef
 
 define MOD_WIRT_INSTALL_TARGET_CMDS_APPSW
-	INSTALL="$(INSTALL)" MOD="$(@D)/artifacts" TARGET_DIR="$(TARGET_DIR)" MACHINE=$(BR2_MACHINE) package/mod/mod-wirt/mod-wirt.sh
+	INSTALL="$(INSTALL)" MOD="$(@D)/artifacts" TARGET_DIR="$(TARGET_DIR)" MACHINE=$(BR2_MACHINE) package/mod/mod-wirt/mod-wirt.sh;
 endef
 
 MOD_WIRT_INSTALL_TARGET_CMDS = $(MOD_WIRT_INSTALL_TARGET_CMDS_BASE)
